2c3396144ba2ef8f2090d95371c06ecdcdb15a40
Marvin Gorecki <marvingorecki@googlemail.com>
2024-02-08 13:55:35 +0700

    feat: increase read progress indicatore height

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -6,10 +6,10 @@ export const NavBar = () => {
   const { scrollYProgress } = useScroll();
 
   return (
-    <nav className="sticky top-0 z-50 overflow-x-hidden pt-1 backdrop-blur">
+    <nav className="sticky top-0 z-50 overflow-x-hidden pt-2 backdrop-blur">
       <motion.div
         style={{ scaleX: scrollYProgress }}
-        className={`fixed inset-0 h-1 w-[105%] origin-left rounded-md bg-progress`}
+        className={`fixed inset-0 h-2 w-[105%] origin-left rounded-md bg-progress`}
       />
     </nav>
   );
